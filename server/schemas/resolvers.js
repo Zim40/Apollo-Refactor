545a7ba147dc87e11729f60985e490a75d8e075b
User <michaelm810129@gmail.com>
2023-05-14 14:31:42 +0930

    refactored Auth.js to handle Apollo server and GraphQl/ previous version utilizing RestAPI

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,19 @@
 const { gql } = require('apollo-server-express');
-
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models/index');
 
 const resolvers = {
-    
-}
+  Query: {
+    me: async (parent, args, context) => {
+      try {
+        const userId = context.user._id;
+        return User.findOne({ _id: userId });
+      } catch {
+        throw new AuthenticationError(
+          "We had trouble Retrieving your Profile. Please try again"
+        );
+      };
+      
+    },
+  },
+};
